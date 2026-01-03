0b0ee0c06200c5391733b1cc245ef9f1f2e2a042
CheryKola <paintballgoose@gmail.com>
2026-01-02 18:37:32 -0600

    Added styling, included hotkey shortcuts for Submit and Clear button

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* the idea, so far, is to display a page that you can type how many dice need to be rolled, then have them get sorted by the rolled value.
    5s would go into one area, 4s, in another, etc */
 let diceRollResults = [];
+let historyResults = [];
 //let onesArr = [];
 //let twosArr = [];
 //let threesArr = [];
@@ -16,13 +17,19 @@ const diceRoller = (x) => {
     for (let i = x; i > 0; i--){
         diceRollResults.unshift(Math.ceil(Math.random() * 6));
     }
-    let stringRoll = diceRollResults.join(' | ');
+    let stringRoll = diceRollResults.join(' , ');
     return stringRoll;
 }
 
 const updateResults = () => {
     let numOfRolls = Number(numOfRollsInput.value);
     results.innerHTML = diceRoller(numOfRolls);
+}
+
+const updateResultsKey = (keypress) =>{
+    if (keypress.key === "Enter"){
+        updateResults();
+    }
 }
 
 /*const sortResults = () => {
@@ -34,5 +41,15 @@ const clearResults = () => {
     results.innerHTML = '';
 }
 
+const clearResultsKey = (keypress) =>{
+    if (keypress.key === "Delete"){
+        clearResults();
+    }
+}
+
 submitButton.addEventListener('click', updateResults);
+//Enter Key for updateResults
+document.addEventListener('keydown', updateResultsKey);
 clearButton.addEventListener('click', clearResults);
+//Delete Key for clearResults
+document.addEventListener('keydown', clearResultsKey); 
